62f09471c5947be6d7d2baefb709475fc05c241c
Pablo Antonioletti <pablo.antonioletti@gmail.com>
2016-10-14 17:49:48 -0300

    Removed crap

--- cpp/forecast_xml_build/build/Debug/Cygwin_4.x-Windows/build_forecast.o.d
+++
@@ -1,1 +0,0 @@
-build/Debug/Cygwin_4.x-Windows/build_forecast.o: build_forecast.cpp
--- cpp/forecast_xml_build/dist/Debug/Cygwin_4.x-Windows/test.sh
+++
@@ -1,1 +0,0 @@
-./forecast_xml_build.exe /cygdrive/c/dev/projects/ScriptsUtil/data/input/forecast_ripleyCL.txt > /cygdrive/c/dev/projects/ScriptsUtil/data/output/test.xml
